c86be52bdace4d0eb1c8ca3de59ae877b1f21306
Kevin Joseph Mangulabnan <wattholm@users.noreply.github.com>
2019-12-07 14:27:13 +0800

    Initial Commit

---
+++ TestCollectionView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestCollectionView
+//
+//  Created by KJEM on 07/12/2019.
+//  Copyright © 2019 KJEM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
